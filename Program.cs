fb77d632f82cc2af3ba83a76b1edbc56b3d1388f
Felix <felixmcwong@gmail.com>
2024-11-11 13:45:47 +1300

    Created method to draw board

--- Program.cs
+++ Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+        }
+
+        static void DrawBoard(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(new string('-', 33));
+
+            for (int i = 0; i < 8; i++)
+            {
+                int yAdd = 2 * i;
+
+                Console.SetCursorPosition(x, y + 1 + yAdd);
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write("|   ");
+                }
+                Console.Write('|');
+
+                if (i == 7) break;
+
+                Console.SetCursorPosition(x, y + 2 + yAdd);
+                Console.Write("|---");
+                for (int j = 0; j < 7; j++)
+                {
+                    Console.Write("+---");
+                }
+                Console.Write('|');
+            }
+
+            Console.SetCursorPosition(x, y + 16);
+            Console.WriteLine(new string('-', 33));
         }
     }
 }
