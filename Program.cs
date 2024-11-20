ca1c316a4754def8e7c6b30d06701a23bd1aa7c9
Felix <felixmcwong@gmail.com>
2024-11-21 11:05:06 +1300

    Edited PieceMap method for better use in drawing pieces and positions

--- Program.cs
+++ Program.cs
@@ -5,12 +5,20 @@
         // True = White, False = Black
         static bool playerSide = true;
         static Dictionary<string, string> pieceMap = new();
+        static string[] pieceList = new string[32];
+        static Dictionary<string, string> pieceAvatars = new();
+        static Dictionary<string, int[]> placemap = new();
         static int x = 0;
         static int y = 0;
 
         static void Main(string[] args)
         {
+            DrawBoard();
+            CreatePieces();
+            CreatePlaceMap();
+            DrawPieces(true);
 
+            Console.ReadKey(true);
         }
 
         static void DrawBoard()
@@ -44,81 +52,133 @@
             Console.WriteLine(new string('-', 33));
         }
 
-        static void CreatePieceMap()
+        static void CreatePieces()
         {
-            if (playerSide == true)
+            pieceMap = new Dictionary<string, string> {
+                {"paw", "a2" },
+                {"pbw", "b2" },
+                {"pcw", "c2" },
+                {"pdw", "d2" },
+                {"pew", "e2" },
+                {"pfw", "f2" },
+                {"pgw", "g2" },
+                {"phw", "h2" },
+                {"rqw", "a1" },
+                {"kqw", "b1" },
+                {"bqw", "c1" },
+                {"quw", "d1" },
+                {"kiw", "e1" },
+                {"bkw", "f1" },
+                {"kkw", "g1" },
+                {"rkw", "h1" },
+                {"pab", "a7" },
+                {"pbb", "b7" },
+                {"pcb", "c7" },
+                {"pdb", "d7" },
+                {"peb", "e7" },
+                {"pfb", "f7" },
+                {"pgb", "g7" },
+                {"phb", "h7" },
+                {"rqb", "a8" },
+                {"kqb", "b8" },
+                {"bqb", "c8" },
+                {"qub", "d8" },
+                {"kib", "e8" },
+                {"bkb", "f8" },
+                {"kkb", "g8" },
+                {"rkb", "h8" }
+            };
+            pieceList = ["paw", "pbw", "pcw", "pdw", "pew", "pfw", "pgw", "phw", "rqw", "kqw", "bqw", "quw", "kiw", "bkw", "kkw", "rkw", "pab", "pbb", "pcb", "pdb", "peb", "pfb", "pgb", "phb", "rqb", "kqb", "bqb", "qub", "kib", "bkb", "kkb", "rkb"];
+            pieceAvatars = new Dictionary<string, string> {
+                {"paw", "P" },
+                {"pbw", "P" },
+                {"pcw", "P" },
+                {"pdw", "P" },
+                {"pew", "P" },
+                {"pfw", "P" },
+                {"pgw", "P" },
+                {"phw", "P" },
+                {"rqw", "R" },
+                {"kqw", "N" },
+                {"bqw", "B" },
+                {"quw", "Q" },
+                {"kiw", "K" },
+                {"bkw", "B" },
+                {"kkw", "N" },
+                {"rkw", "R" },
+                {"pab", "P*" },
+                {"pbb", "P*" },
+                {"pcb", "P*" },
+                {"pdb", "P*" },
+                {"peb", "P*" },
+                {"pfb", "P*" },
+                {"pgb", "P*" },
+                {"phb", "P*" },
+                {"rqb", "R*" },
+                {"kqb", "N*" },
+                {"bqb", "B*" },
+                {"qub", "Q*" },
+                {"kib", "K*" },
+                {"bkb", "B*" },
+                {"kkb", "N*" },
+                {"rkb", "R*" }
+            };
+        }
+
+        static void CreatePlaceMap()
+        {
+            for (int i = 0; i < 8; i++)
             {
-                pieceMap = new Dictionary<string, string> {
-                    {"paw", "a2" },
-                    {"pbw", "b2" },
-                    {"pcw", "c2" },
-                    {"pdw", "d2" },
-                    {"pew", "e2" },
-                    {"pfw", "f2" },
-                    {"pgw", "g2" },
-                    {"phw", "h2" },
-                    {"rqw", "a1" },
-                    {"kqw", "b1" },
-                    {"bqw", "c1" },
-                    {"quw", "d1" },
-                    {"kiw", "e1" },
-                    {"kbw", "f1" },
-                    {"kkw", "g1" },
-                    {"krw", "h1" },
-                    {"pab", "a7" },
-                    {"pbb", "b7" },
-                    {"pcb", "c7" },
-                    {"pdb", "d7" },
-                    {"peb", "e7" },
-                    {"pfb", "f7" },
-                    {"pgb", "g7" },
-                    {"phb", "h7" },
-                    {"rqb", "a8" },
-                    {"kqb", "b8" },
-                    {"bqb", "c8" },
-                    {"qub", "d8" },
-                    {"kib", "e8" },
-                    {"kbb", "f8" },
-                    {"kkb", "g8" },
-                    {"krb", "h8" }
-                };
+                for (int j = 0; j < 8; j++)
+                {
+                    if (playerSide == false)
+                    {
+                        char col = (char)('a' + j);
+                        int row = 1 + i;
+                        int[] coord = [2 + (j * 4), (i * 2) + 1];
+
+                        placemap.Add(col.ToString() + row.ToString(), coord);
+                    }
+                    else
+                    {
+                        char col = (char)('h' - j);
+                        int row = 8 - i;
+                        int[] coord = [2 + (j * 4), (i * 2) + 1];
+
+                        placemap.Add(col.ToString() + row.ToString(), coord);
+                    }
+                }
             }
-            else
+        }
+
+        static void DrawPieces(bool init = false)
+        {
+            if (init == false)
             {
-                pieceMap = new Dictionary<string, string> {
-                    {"pab", "a2" },
-                    {"pbb", "b2" },
-                    {"pcb", "c2" },
-                    {"pdb", "d2" },
-                    {"peb", "e2" },
-                    {"pfb", "f2" },
-                    {"pgb", "g2" },
-                    {"phb", "h2" },
-                    {"rqb", "a1" },
-                    {"kqb", "b1" },
-                    {"bqb", "c1" },
-                    {"qub", "d1" },
-                    {"kib", "e1" },
-                    {"kbb", "f1" },
-                    {"kkb", "g1" },
-                    {"krb", "h1" },
-                    {"paw", "a7" },
-                    {"pbw", "b7" },
-                    {"pcw", "c7" },
-                    {"pdw", "d7" },
-                    {"pew", "e7" },
-                    {"pfw", "f7" },
-                    {"pgw", "g7" },
-                    {"phw", "h7" },
-                    {"rqw", "a8" },
-                    {"kqw", "b8" },
-                    {"bqw", "c8" },
-                    {"quw", "d8" },
-                    {"kiw", "e8" },
-                    {"kbw", "f8" },
-                    {"kkw", "g8" },
-                    {"krw", "h8" }
-                };
+                for (int i = 0; i < 8; i++)
+                {
+                    int yAdd = (i * 2) + 1;
+
+                    for (int j = 0; j < 8; j++)
+                    {
+                        int xAdd1 = 2 + (j * 4);
+                        int xAdd2 = 3 + (j * 4);
+
+                        Console.SetCursorPosition(x + xAdd1, y + yAdd);
+                        Console.Write(' ');
+                        Console.SetCursorPosition(x + xAdd2, y + yAdd);
+                        Console.Write(' ');
+                    }
+                }
+            }
+
+            foreach (string piece in pieceList)
+            {
+                string coord = pieceMap[piece];
+                int[] cursorPosition = placemap[coord];
+                string pieceAvatar = pieceAvatars[piece];
+                Console.SetCursorPosition(cursorPosition[0], cursorPosition[1]);
+                Console.Write(pieceAvatar);
             }
         }
     }
